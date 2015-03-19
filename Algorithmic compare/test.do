511a2e6f7146bab4c90fdfc8551d64c9b4b737bb
RNA 1991 <robinsmeel@hotmail.com>
2015-03-19 09:55:07 +0100

    Stap 2

---
+++ Algorithmic compare/test.do
@@ -0,0 +1,19 @@
+vsim work.topentity
+add wave *
+
+add wave -position end  sim:/topentity/proc/state
+add wave -position end  sim:/topentity/proc/reg
+add wave -position end  sim:/topentity/proc/reg_LO
+add wave -position end  sim:/topentity/proc/reg_HI
+add wave -position end  sim:/topentity/mem/mem
+
+radix signal sim:/topentity/address_bus hexadecimal
+radix signal sim:/topentity/databus1 hexadecimal
+radix signal sim:/topentity/databus2 hexadecimal
+
+
+force clk 0,1 10ns -repeat 20ns
+force reset 1
+run 50ns
+force reset 0
+run 1000ns
---
+++ Algorithmic compare/topEntity.vhd
@@ -0,0 +1,67 @@
+--------------------------------------------------------------
+-- 
+-- Behavioral description
+--
+--------------------------------------------------------------
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.ALL;
+
+ENTITY topEntity IS
+	PORT(
+		reset 	: IN std_logic;
+		clk		: IN std_logic
+	);
+END topEntity;
+
+ARCHITECTURE behaviour OF topEntity IS
+	
+	component processor
+		PORT(
+			address_bus	: OUT std_logic_vector(31 DOWNTO 0);
+			databus_in	: IN  std_logic_vector(31 DOWNTO 0);
+			databus_out : OUT std_logic_vector(31 DOWNTO 0);
+			write 		: OUT std_logic;
+			reset		: IN  std_logic;
+			clk			: IN  std_logic
+		);
+	END component;
+
+	component memory
+		PORT(
+			address_bus	: IN  std_logic_vector(31 DOWNTO 0);
+			databus_in	: IN  std_logic_vector(31 DOWNTO 0);
+			databus_out : OUT std_logic_vector(31 DOWNTO 0);
+			write 		: IN  std_logic;	
+			clk			: IN  std_logic
+			);
+	END component;
+
+	SIGNAL address_bus	: std_logic_vector(31 DOWNTO 0);
+	SIGNAL databus1		: std_logic_vector(31 DOWNTO 0);
+	SIGNAL databus2 	: std_logic_vector(31 DOWNTO 0);
+	SIGNAL write 		: std_logic;
+
+	BEGIN
+		proc : processor
+		PORT MAP(
+				address_bus => address_bus,
+				databus_in	=> databus1,
+				databus_out => databus2,
+				write		=> write,
+				reset		=> reset,
+				clk			=> clk
+				);
+
+		mem : memory
+		PORT MAP(
+				address_bus => address_bus,
+				databus_in	=> databus2,
+				databus_out => databus1,
+				write		=> write,
+				clk			=> clk
+				);
+
+END ARCHITECTURE;
