e152c13be971a97e7006a4ed5ad3b6fbb8153d10
markieb1 <mark_beekhof@hotmail.com>
2015-03-26 12:00:06 +0100

    Error added to compare.vhd when bevhaviourial and algorithmic processor do different things

--- Algorithmic compare/compare.vhd
+++ Algorithmic compare/compare.vhd
@@ -28,15 +28,19 @@ ARCHITECTURE behaviour OF compare IS
 
 BEGIN
 	PROCESS(address_bus_bhv,address_bus_alg)
+		VARIABLE previous_data_on_bus          : std_logic_vector(31 DOWNTO 0);
 	BEGIN
 		IF address_bus_bhv = address_bus_alg THEN
-			address_bus_mem <= address_bus_bhv;
+			address_bus_mem 	<= address_bus_bhv;
 			write_mem 		<= write_bhv;
 			enable_bhv 		<= '1';
+			previous_data_on_bus 	:= address_bus_bhv;
+		ELSIF address_bus_alg = previous_data_on_bus THEN
+			enable_bhv 		<= '0';
 		ELSE
+			ASSERT false REPORT "Compare error!" SEVERITY failure ;
 			enable_bhv 		<= '0';
 		END IF;
-
 	END PROCESS;
 
 END ARCHITECTURE;
