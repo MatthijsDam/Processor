5f783989d0e662dcf4e53e1e51839121d5c3754f
markieb1 <mark_beekhof@hotmail.com>
2015-03-31 11:12:26 +0200

    Compare controleert nu ook wat er op de databus staat in plaats van alleen het adres te controleren

--- Algorithmic compare/compare.vhd
+++ Algorithmic compare/compare.vhd
@@ -29,13 +29,15 @@ ARCHITECTURE behaviour OF compare IS
 BEGIN
 	PROCESS(address_bus_bhv,address_bus_alg)
 		VARIABLE previous_data_on_bus          : std_logic_vector(31 DOWNTO 0);
+		VARIABLE previous_address_on_bus	: std_logic_vector(31 DOWNTO 0);
 	BEGIN
-		IF address_bus_bhv = address_bus_alg THEN
+		IF address_bus_bhv = address_bus_alg AND data_bus_alg = data_bus_bhv THEN
 			address_bus_mem 	<= address_bus_bhv;
 			write_mem 		<= write_bhv;
 			enable_bhv 		<= '1';
-			previous_data_on_bus 	:= address_bus_bhv;
-		ELSIF address_bus_alg = previous_data_on_bus THEN
+			previous_address_on_bus 	:= address_bus_alg;
+			previous_data_on_bus		:= data_bus_alg;
+		ELSIF address_bus_alg = previous_address_on_bus AND data_bus_alg = previous_data_on_bus THEN
 			enable_bhv 		<= '0';
 		ELSE
 			ASSERT false REPORT "Compare error!" SEVERITY failure ;
