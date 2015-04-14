d41d3a302848f03dff8a0aa1a9a08a81f66bd5e3
Leon Schoorl <l.m.schoorl@student.utwente.nl>
2015-04-14 11:05:11 +0200

    Improved assemble-to-mem-vhdl.hs

--- Test_prog/assemble-to-mem-vhdl.hs
+++ Test_prog/assemble-to-mem-vhdl.hs
@@ -4,6 +4,8 @@ import System.Environment
 import System.Process
 import System.Exit
 import System.Directory
+import Data.Time
+import Data.List
 
 findMars :: IO String
 findMars = do
@@ -16,31 +18,44 @@ findMars = do
 assemble inputfile outputfile = do
 	mars <- findMars
 	printfProgressLn "running mars to assemble %s to %s" inputfile outputfile
-	rawSystem "java" ["-jar",mars, "a", "ae1", "dump", ".text", "BinaryText", outputfile, inputfile]
+	let runMars dumptype outputfile = rawSystem "java" ["-jar",mars, "a", "ae1",  "mc","CompactTextAtZero",  "dump", ".text", dumptype, outputfile, inputfile]
+	runMars "BinaryText" outputfile
+	runMars "SegmentWindow" (outputfile ++ "2")
 
-header = "LIBRARY ieee;\nUSE ieee.std_logic_1164.ALL;\nUSE ieee.numeric_std.ALL;\nPACKAGE program IS\n\tCONSTANT low_address\t: INTEGER := 0; \n\tCONSTANT high_address\t: INTEGER := 255;\n\n\tTYPE mem_array IS ARRAY (low_address TO high_address) OF std_logic_vector(31 DOWNTO 0);\n\tCONSTANT program : mem_array := (\n"
+memsize = 2 -- 55
+header = printf "LIBRARY ieee;\nUSE ieee.std_logic_1164.ALL;\nUSE ieee.numeric_std.ALL;\nPACKAGE program IS\n\tCONSTANT low_address\t: INTEGER := 0; \n\tCONSTANT high_address\t: INTEGER := %d;\n\n\tTYPE mem_array IS ARRAY (low_address TO high_address) OF std_logic_vector(31 DOWNTO 0);\n\tCONSTANT program : mem_array := (\n" memsize
 footer = "\nOTHERS => \"00000000000000000000000000000000\");\n\nEND;\n"
 translateMars2VHDL str = header ++ (unlines $ map (printf "\"%s\",") (lines str)) ++ footer
+
 
 main = do
 	args <- getArgs
 	let
 		[file] = args
-		outfile = "../Algorithmic compare/program.vhd"
+		outfiles = ["../Algorithmic compare/program.vhd", "../Task3/program.vhd"]
 		tempfile = file ++ ".mars-out"
+		tempfile2 = file ++ ".mars-out2"
 	checkFile file
-	checkFile outfile
+	mapM_ checkFile outfiles
 	resMars <- assemble file tempfile
+	time <- getZonedTime
 	case resMars of
 		ExitFailure _ -> exitFailure
 		ExitSuccess -> do
-			str <- readFile tempfile
-			let output = (printf "-- assembled from %s\n" file) ++ translateMars2VHDL str
-			printfProgressLn "writing to %s" outfile
-			writeFile outfile output
+			prg <- readFile tempfile
+			source <- readFile tempfile2
+			let progsize = length (lines prg)
+			    line = replicate 40 '!' ++ "\n"
+			if progsize > memsize then printfProgress (line++"WARNING: program to big for memory\n"++line) else return ()
+			let output = (printf "-- assembled from %s on %s\n\n" file (show time)) ++ comment source ++ translateMars2VHDL prg
+			mapM_ (printfProgressLn "writing to %s") outfiles
+			mapM_ (\f -> writeFile f output) outfiles
 			printfProgressLn "removing %s" tempfile
 			removeFile tempfile
+			removeFile tempfile2
 			return ()
+
+comment str = unlines (map ("-- " ++) (lines str))
 
 checkFile f = do
 	exists <- doesFileExist f
